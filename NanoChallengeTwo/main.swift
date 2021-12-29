585475ff113ec0936496ee2b77de27588a86d3dc
Esra Alhussaini <alhussaini.esra@gmail.com>
2021-12-29 12:09:39 +0300

    Initial Commit

---
+++ NanoChallengeTwo/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  NanoChallengeTwo
+//
+//  Created by ESRA ALHUSSAINI on 28/12/2021.
+//
+
+import Foundation
+
+print("""
+        Hello, Choose one of the books below for rental \n
+        ---------------------------------------------\n
+        Press 1 if you know the book title,otherwise Press 2\n
+        """)
+
+var userChoise = readLine()
+
+//if userChoise == 1 {
+//    print("you chose 1")
+    
+//    else
+//    print("you choose 2")
+
+      //  }
+print(userChoise) 
